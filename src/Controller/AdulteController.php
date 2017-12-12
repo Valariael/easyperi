1bf9bd87d964eecfae29ecf4a57c789ffd72b8a3
tomdelacote <tom.delacote@gmail.com>
2017-12-12 20:14:22 +0100

    Add files via upload

--- src/Controller/AdulteController.php
+++ src/Controller/AdulteController.php
@@ -13,8 +13,8 @@ class AdulteController implements ControllerProviderInterface{
     private $adulteModel;
 
     public function validFormAdd(Application $app){
-        if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['nomVille'])
-            && isset($_POST['adresse']) && isset($_POST['codePostal']) && isset($_POST['telephone'])
+        if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['ville'])
+            && isset($_POST['adresse']) && isset($_POST['code_postal']) && isset($_POST['telephone'])
             && isset($_POST['adresseMail']) && isset($_POST['password'])) {
 
             $donnees = $this->getData($_POST);
@@ -53,13 +53,12 @@ class AdulteController implements ControllerProviderInterface{
         }
         $data['nom']=htmlentities($post['nom']);
         $data['prenom']=htmlentities($post['prenom']);
-        $data['nomVille']=htmlentities($post['nomVille']);
-        $data['codePostal']=htmlentities($post['codePostal']);
+        $data['ville']=htmlentities($post['ville']);
+        $data['code_postal']=htmlentities($post['code_postal']);
         $data['adresse']=htmlentities($post['adresse']);
         $data['telephone']=htmlentities($post['telephone']);
         $data['adresseMail']=htmlentities($post['adresseMail']);
-        $data['codePostal']=$post['codePostal'];
-        $data['password']=$post['password'];
+        $data['password']=htmlentities($post['password']);
         return $data;
     }
 
@@ -67,8 +66,8 @@ class AdulteController implements ControllerProviderInterface{
         $erreurs = [];
         if ((! preg_match("/^[A-Za-z ]{2,}/",$donnees['nom']))) $erreurs['nom']='nom composé de 2 lettres minimum';
         if ((! preg_match("/^[A-Za-z ]{2,}/",$donnees['prenom']))) $erreurs['prenom']='prenom composé de 2 lettres minimum';
-        if ((! preg_match("/^[A-Za-z ]{2,}/",$donnees['nomVille']))) $erreurs['nomVille']='ville composé de 2 lettres minimum';
-        if ((! preg_match("/^[0-9]{5}/",$donnees['codePostal']))) $erreurs['codePostal']='le code postal doit être composé de 5 caractères numériques';
+        if ((! preg_match("/^[A-Za-z ]{2,}/",$donnees['ville']))) $erreurs['ville']='ville composé de 2 lettres minimum';
+        if ((! preg_match("/^[0-9]{5}/",$donnees['code_postal']))) $erreurs['code_postal']='le code postal doit être composé de 5 caractères numériques';
         if ((! preg_match("/^[\w.-]+@[\w.-]+\.[a-z]{2,6}$/",$donnees['adresseMail']))) $erreurs['adresseMail']='veuillez entrer une adresse mail valide';
         if ((! preg_match("/^[0-9]{10}/",$donnees['telephone']))) $erreurs['telephone']='veuillez entrer un numéro de téléphone valide';
 
@@ -99,6 +98,8 @@ class AdulteController implements ControllerProviderInterface{
         (new AdulteModel($app))->deleteAdulte($id);
         return $this->show($app);
     }
+
+    #______________ CONNEXION ____________________________
 
     public function connexionAdulte(Application $app)
     {
@@ -136,6 +137,75 @@ class AdulteController implements ControllerProviderInterface{
         return $app->redirect($app["url_generator"]->generate("adulte.show"));
     }
 
+    #______________ INFOS ____________________________
+
+    public function showInfos(Application $app) {
+        $this->adulteModel = new AdulteModel($app);
+        $idAdulte = $app['session']->get('idAdulte');
+        $donnees = $this->adulteModel->getAdulte($idAdulte);
+        $donnees['telephone'] = chunk_split($donnees['telephone'], 2, ' ');
+        return $app["twig"]->render('famille/adulte/showInfos.html.twig', ['donnees'=>$donnees]);
+    }
+
+    public function editInfos(Application $app) {
+        $idAdulte = $app['session']->get('idAdulte');
+        $this->adulteModel = new AdulteModel($app);
+        $donnees = $this->adulteModel->getAdulte($idAdulte);
+        return $app["twig"]->render('famille/adulte/editInfos.html.twig',['donnees'=>$donnees]);
+    }
+
+    public function testeDonneesModifieesAdulte($app, $donnees, $old_username) {
+        $this->adulteModel = new AdulteModel($app);
+        $erreurs = [];
+        if ((! preg_match("/[A-Za-z0-9]{5,}/",$donnees['username']))) $erreurs['username']= 'Nom d\'utilisateur composé de 5 caractères minimum';
+        if (! $donnees['username'] == $old_username) {
+            if (!$this->adulteModel->isUserNameAvailable($donnees['username'])) $erreurs['username'] = 'Nom d\'utilisateur déjà pris';
+        }
+        if (!filter_var($donnees['adresseMail'], FILTER_VALIDATE_EMAIL)) { $erreurs['adresseMail']= 'Adresse mail incorrecte'; }
+        if ((! preg_match("/[A-Za-z ]{2,}/",$donnees['ville']))) $erreurs['ville']= 'Ville composée de deux lettres minimum';
+        if ((! preg_match("/[A-Za-z0-9 ]{2,}/",$donnees['adresse']))) $erreurs['adresse']= 'Adresse composée de deux caractères minimum';
+        if ((! preg_match("/[A-Za-z ]{2,}/",$donnees['nom']))) $erreurs['nom']= 'Nom composé de deux lettres minimum';
+        if ((! preg_match("/[A-Za-z ]{2,}/",$donnees['prenom']))) $erreurs['prenom']= 'Prénom composé de lettres minimum';
+        if ((!preg_match("/^[0-9]{5}$/", $donnees['code_postal']))) $erreurs['code_postal']= 'Code postal composé de 5 chiffres';
+        if ((!preg_match("/^[0-9]{10}$/", $donnees['telephone']))) $erreurs['telephone']= 'Téléphone composé de 10 chiffres';
+
+        return $erreurs;
+    }
+
+    public function validFormEditInfos(Application $app) {
+        $this->adulteModel = new AdulteModel($app);
+        $erreurs = [];
+        $idAdulte = $app['session']->get('idAdulte');
+        $username = $app['session']->get('username');
+        $donnees = [
+            'username' => htmlspecialchars($_POST['username']),
+            'adresseMail' => htmlspecialchars($_POST['adresseMail']),
+            'nom' => htmlspecialchars($_POST['nom']),
+            'prenom' => htmlspecialchars($_POST['prenom']),
+            'code_postal' => htmlspecialchars($_POST['code_postal']),
+            'ville' => htmlspecialchars($_POST['ville']),
+            'adresse' => htmlspecialchars($_POST['adresse']),
+            'password' => htmlspecialchars($_POST['password']),
+            'telephone' => htmlspecialchars($_POST['telephone'])
+        ];
+
+        $erreurs = $this->testeDonneesModifieesAdulte($app, $donnees, $username);
+        $password_wrong = !$this->adulteModel->loginCheckAdulte($username, $donnees['password']);
+
+        if (!empty($erreurs) or $password_wrong) {
+            if ($password_wrong) $erreurs['password'] = 'Mot de passe incorrect';
+            return $app["twig"]->render('famille/adulte/editInfos.html.twig',['donnees'=>$donnees,'erreurs'=>$erreurs]);
+        }
+        else
+        {
+            // changer app session username !
+            $app['session']->set('username', $donnees['username']);
+            $this->adulteModel->updateAdulte($idAdulte, $donnees);
+            return $app->redirect($app["url_generator"]->generate("adulte.showInfos"));
+        }
+
+    }
+
     public function connect(Application $app)
     {
         $controllers = $app['controllers_factory'];
@@ -149,6 +219,10 @@ class AdulteController implements ControllerProviderInterface{
         $controllers->get('/errorAdulte', 'App\Controller\AdulteController::errorDroit')->bind('adulte.erreurs');
 
         $controllers->get('/showAdulte', 'App\Controller\AdulteController::show')->bind('adulte.show');
+
+        $controllers->get('/infos', 'App\Controller\AdulteController::showInfos')->bind('adulte.showInfos');
+        $controllers->get('/infos/edit', 'App\Controller\AdulteController::editInfos')->bind('adulte.editInfos');
+        $controllers->post('/infos/edit', 'App\Controller\AdulteController::validFormEditInfos')->bind('adulte.validEditInfos');
 
         $controllers->get('/addAdulte', 'App\Controller\AdulteController::add')->bind('adulte.add');
         $controllers->get('/addResp/{id}', 'App\Controller\AdulteController::addResp')->bind('adulte.addResp');

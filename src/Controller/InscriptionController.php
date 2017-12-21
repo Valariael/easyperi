cff029825855577e369d8f929282e3f5e06ddc67
tlacaill1 <34522584+tlacaill1@users.noreply.github.com>
2017-12-21 13:33:38 +0100

    merge a la main
    ça devrait marcher

---
+++ src/Controller/InscriptionController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tlacaill
+ * Date: 15/12/17
+ * Time: 10:58
+ */
+
+namespace App\Controller;
+
+
+use App\Model\InscriptionModel;
+use Silex\Api\ControllerProviderInterface;
+use Silex\Application;
+use Silex\ControllerCollection;
+
+class InscriptionController implements ControllerProviderInterface
+{
+
+    public function showInscriptions(Application $app, $id)
+    {
+        $donnees = (new InscriptionModel($app))->getEnfantsByIdAgenda($id);
+        return $app["twig"]->render('inscription/show.html.twig', compact('donnees'));
+    }
+
+    public function connect(Application $app)
+    {
+        $controllers = $app['controllers_factory'];
+        $controllers->get('/showInscriptions/{id}', 'App\Controller\InscriptionController::showInscriptions')->bind('inscription.showInscriptions');
+        $controllers->match('/showInscriptions/{id}', 'App\Controller\InscriptionController::showInscriptions')->bind('inscription.showInscriptions');
+        return $controllers;
+    }
+}
